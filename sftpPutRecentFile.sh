7ea39d58535870fd633869cbb0a049f3ed3a756c
IroiKanta <e261.whydunit.62305@gmail.com>
2023-03-12 11:56:33 +0900

    bash でも zsh でも .sh 自身の場所を読み取れるようにした

--- sftpPutRecentFile.sh
+++ sftpPutRecentFile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 設定ファイルからファイル検索場所とsftp接続先の情報を取得する
-source "$(cd "$(dirname "$0")"; pwd)/config.cfg"
+source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/config.cfg
 # config.cfgは以下のように書いてください
 # DIRECTORY=/path/to/directory
 # HOST=user@host
@@ -11,6 +11,7 @@ source "$(cd "$(dirname "$0")"; pwd)/config.cfg"
 # ディレクトリを移動して最新のzipファイルを探す
 cd "$DIRECTORY"
 latest_zip=$(find . -name '*.zip' -type f -printf '%T+ %p\n' | sort | tail -n 1 | awk '{print $2}')
+echo $latest_zip を送信します
 
 # sftp経由でzipファイルを送信
 sftp -i "$PRIVATE_KEY" "$HOST"
