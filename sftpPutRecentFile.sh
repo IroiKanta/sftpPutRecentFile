2a42b3d04388fe28847882a0e6565dcdb80be5e8
IroiKanta <e261.whydunit.62305@gmail.com>
2023-03-12 12:26:25 +0900

    source コマンドを bash 用に置き換え

--- sftpPutRecentFile.sh
+++ sftpPutRecentFile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # 設定ファイルからファイル検索場所とsftp接続先の情報を取得する
-source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/config.cfg
+# bash では source コマンドがないのでこれで実行
+. $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/config.cfg
 # config.cfgは以下のように書いてください
 # DIRECTORY=/path/to/directory
 # HOST=user@host
@@ -9,7 +10,7 @@ source $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/config.cfg
 # PRIVATE_KEY=/path/to/private/key
 
 # ディレクトリを移動して最新のzipファイルを探す
-cd "$DIRECTORY"
+cd $DIRECTORY
 latest_zip=$(find . -name '*.zip' -type f -printf '%T+ %p\n' | sort | tail -n 1 | awk '{print $2}')
 echo $latest_zip を送信します
 
