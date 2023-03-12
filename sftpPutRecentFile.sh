bb0e06c109e5b20e0708a85da10918ef5af7024c
IroiKanta <e261.whydunit.62305@gmail.com>
2023-03-12 10:40:48 +0900

    コード作成
    .sh 初版作成

---
+++ sftpPutRecentFile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 設定ファイルからファイル検索場所とsftp接続先の情報を取得する
+source "$(cd "$(dirname "$0")"; pwd)/config.cfg"
+# config.cfgは以下のように書いてください
+# DIRECTORY=/path/to/directory
+# HOST=user@host
+# REMOTE_DIR=/remote/directory/
+# PRIVATE_KEY=/path/to/private/key
+
+# ディレクトリを移動して最新のzipファイルを探す
+cd "$DIRECTORY"
+latest_zip=$(find . -name '*.zip' -type f -printf '%T+ %p\n' | sort | tail -n 1 | awk '{print $2}')
+
+# sftp経由でzipファイルを送信
+sftp -i "$PRIVATE_KEY" "$HOST"
+put "$latest_zip" "$REMOTE_DIR"
