36b7c579da32fd2becf14ae3f32725e02ba752f1
IroiKanta <e261.whydunit.62305@gmail.com>
2023-03-12 12:43:21 +0900

    送信するファイルを設定で指定するように変更

--- sftpPutRecentFile.sh
+++ sftpPutRecentFile.sh
@@ -5,13 +5,14 @@
 . $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/config.cfg
 # config.cfgは以下のように書いてください
 # DIRECTORY=/path/to/directory
+# SEARCH_FILE=target
 # HOST=user@host
 # REMOTE_DIR=/remote/directory/
 # PRIVATE_KEY=/path/to/private/key
 
 # ディレクトリを移動して最新のzipファイルを探す
 cd $DIRECTORY
-latest_zip=$(find . -name '*.zip' -type f -printf '%T+ %p\n' | sort | tail -n 1 | awk '{print $2}')
+latest_zip=$(find . -name "$SEARCH_FILE" -type f -printf '%T+ %p\n' | sort | tail -n 1 | awk '{print $2}')
 echo $latest_zip を送信します
 
 # sftp経由でzipファイルを送信
